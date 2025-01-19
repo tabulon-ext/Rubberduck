5ff04bffccec1ac00335b31f543301abbbd2ceda
Mathieu Guindon <mathieu.guindon@live.ca>
2025-01-18 21:19:42 -0500

    added remarks

--- Rubberduck.CodeAnalysis/Inspections/Concrete/ThunderCode/NonBreakingSpaceIdentifierInspection.cs
+++ Rubberduck.CodeAnalysis/Inspections/Concrete/ThunderCode/NonBreakingSpaceIdentifierInspection.cs
@@ -14,13 +14,16 @@ namespace Rubberduck.CodeAnalysis.Inspections.Concrete.ThunderCode
     /// code our friend Andrew Jackson would have written to confuse Rubberduck's parser and/or resolver. 
     /// This inspection may accidentally reveal non-breaking spaces in code copied and pasted from a website.
     /// </why>
+    /// <remarks>
+    /// You may have discovered this inspection by pasting code directly from a web page, which often contains such non-printable characters.
+    /// </remarks>
     internal sealed class NonBreakingSpaceIdentifierInspection : DeclarationInspectionBase
     {
         private const string Nbsp = "\u00A0";
 
         public NonBreakingSpaceIdentifierInspection(IDeclarationFinderProvider declarationFinderProvider)
             : base(declarationFinderProvider)
-        {}
+        { }
 
         protected override bool IsResultDeclaration(Declaration declaration, DeclarationFinder finder)
         {
